03be785f700134fc87fcde977a15c907bab89ab4
Anvarito <anvaritohaz@gmail.com>
2019-02-18 18:00:18 +0500

    Реализована стрельба от анимации

--- Assets/Scripts/Weapons/MachineGun.cs
+++ Assets/Scripts/Weapons/MachineGun.cs
@@ -9,6 +9,14 @@ namespace Weapons
 
 		public override void Fire()
 		{
+		
+			animator.Play("Fire");
+			
+			
+		}
+		
+		
+		public void shoot(){
 			var newBullet = Instantiate(bullet, bulletSpawnPosition.position, Quaternion.identity);
 			var LaunchVector = bulletSpawnPosition.position;
 			var direction = bulletSpawnPosition.lossyScale.x < 0 ? Vector2.left : Vector2.right;
