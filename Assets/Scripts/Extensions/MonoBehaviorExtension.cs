88aa6bcf4d91de186aab0c7b0053f6e11fd7aab8
Wilhelm <nightmare.v.94@gmail.com>
2019-02-22 13:56:09 +0500

    Добавлены комментарии

--- Assets/Scripts/Extensions/MonoBehaviorExtension.cs
+++ Assets/Scripts/Extensions/MonoBehaviorExtension.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 namespace Extensions
 {
+	/// <summary>
+	/// Класс, расширяющий MonoBehavior
+	/// </summary>
 	public static class MonoBehaviorExtension
 	{
-		private static void Flip(this MonoBehaviour monoBehaviour)
+		/// <summary>
+		/// Развернуть Transform в противоположную сторону
+		/// </summary>
+		public static void Flip(this MonoBehaviour monoBehaviour)
 		{
 			var transform = monoBehaviour.transform;
 			var newScale = transform.localScale;
@@ -12,17 +18,26 @@ namespace Extensions
 			transform.localScale = newScale;
 		}
 
+		/// <summary>
+		/// Повернут ли Transform направо
+		/// </summary>
 		public static bool IsFacingRight(this MonoBehaviour monoBehaviour)
 		{
 			return monoBehaviour.transform.localScale.x > 0;
 		}
 
+		/// <summary>
+		/// Развернуть Transform направо
+		/// </summary>
 		public static void TurnRight(this MonoBehaviour monoBehaviour)
 		{
 			if (monoBehaviour.IsFacingRight()) return;
 			monoBehaviour.Flip();
 		}
 
+		/// <summary>
+		/// Развернуть Transform налево
+		/// </summary>
 		public static void TurnLeft(this MonoBehaviour monoBehaviour)
 		{
 			if (!monoBehaviour.IsFacingRight()) return;
